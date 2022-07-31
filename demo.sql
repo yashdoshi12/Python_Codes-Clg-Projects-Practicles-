e73b21da88029f48591ff3b56d649bdc983e2f65
Yash Doshi <39629707+yashdoshi12@users.noreply.github.com>
2022-07-31 20:39:00 +0530

    Delete demo.sql

--- demo.sql
+++
@@ -1,6 +0,0 @@
-create database demo;
-use demo;
-create user 'sahiil' @'localhost'IDENTIFIED BY '3000';
-grant all on demo.*to 'sahiil'@'localhost';
-create table student(PRN varchar(12),First_Name varchar(20),Middle_Name varchar(20),Last_Name varchar(20),Address varchar(30),
-Mobile_Number int, Email_ID varchar(20),DOB date);
